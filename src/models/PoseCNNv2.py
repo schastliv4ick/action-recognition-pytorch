056052af2d24134e43dcebad79cadbe19c7fcc40
Semyon Karaban <karabans25@sch57.ru>
2025-04-16 21:01:34 +0300

    new model in posecnnv2

--- src/models/PoseCNNv2.py
+++ src/models/PoseCNNv2.py
@@ -1,55 +1,35 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SEBlock(nn.Module):
-    def __init__(self, channels, reduction=16):
-        super(SEBlock, self).__init__()
-        self.se = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),                   
-            nn.Conv2d(channels, channels // reduction, 1),  
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channels // reduction, channels, 1),  
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        scale = self.se(x)
-        return x * scale
-
-
-
-class PoseCNNv2(nn.Module):
+class PoseCNN(nn.Module):
     def __init__(self, num_classes=20):
-        super(PoseCNNv2, self).__init__()
+        super(PoseCNN, self).__init__()
         
-        def conv_block(in_channels, out_channels):
-            return nn.Sequential(
+        def conv_block(in_channels, out_channels, use_dropout=False):
+            layers = [
                 nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
                 nn.BatchNorm2d(out_channels),
                 nn.ReLU(inplace=True),
-                SEBlock(out_channels),          
                 nn.MaxPool2d(2)
-            )
-
+            ]
+            if use_dropout:
+                layers.append(nn.Dropout2d(0.3)) 
+            return nn.Sequential(*layers)
         
         self.backbone = nn.Sequential(
-            conv_block(3, 48),      # 144×256
-            conv_block(48, 96),     # 72×128
-            conv_block(96, 192),    # 36×64
-            conv_block(192, 384),   # 18×32
-            conv_block(384, 768),   # 9×16
+        conv_block(3, 32),
+        conv_block(32, 64),
+        conv_block(64, 128, use_dropout=True),
+        conv_block(128, 256, use_dropout=True),
+        conv_block(256, 512, use_dropout=True),
         )
-
         
         self.pool = nn.AdaptiveAvgPool2d(1)  # → (B, 512, 1, 1)
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(768, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
             nn.Linear(512, 256),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.3),
+            nn.Dropout(0.5),
             nn.Linear(256, num_classes)
         )
 
