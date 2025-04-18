2aed54f92e4d00f9e20801ae9bcaa279d0757598
GrygoryZach <grishazschepilo@gmail.com>
2025-04-18 22:20:59 +0200

    __all_models.py updated

--- src/models/__all_models.py
+++ src/models/__all_models.py
@@ -4,3 +4,5 @@ from .simplierYOLO import EnhancedYOLOLike
 from .PoseCNN import PoseCNN
 from .PoseCNNv2 import PoseCNNv2
 from .PoseCNNsc import PoseCNNsc
+from .PoseCNNsc_13_35 import PoseCNNsc_13_35
+from .PoseCNNsc_13_24_35 import PoseCNNsc_13_24_35
