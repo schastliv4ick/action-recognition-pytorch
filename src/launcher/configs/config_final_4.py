89439891cf88f05e40a7284f953a95ab6ad0bc41
Vlad-Akimov <121798983+Vlad-Akimov@users.noreply.github.com>
2025-04-22 02:40:56 +0300

    Vlad's models with class exclusion

---
+++ src/launcher/configs/config_final_4.py
@@ -0,0 +1,15 @@
+PATH_TO_DATA = "V:\\ML\\yandex-ml-2025\\data"
+RESULT_DIR = "V:\\ML\\yandex-ml-2025\\src\\saves\\PoseCNNsc_13_24_35_#test1"
+SAVE_DIR = "V:\\ML\\yandex-ml-2025\\src\\saves\\PoseCNNsc_13_24_35_#test1"
+
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+NUM_EPOCHS = 100
+
+SCHEDULER = 'CosineAnnealingWarmRestarts'
+
+WEIGHT_DECAY = 1e-2
+
+TRAIN_AUGMENTATION_TYPE = "basic"
+VALID_AUGMENTATION_TYPE = None
---
+++ src/launcher/configs/config_final_5.py
@@ -0,0 +1,15 @@
+PATH_TO_DATA = "V:\\ML\\yandex-ml-2025\\data"
+RESULT_DIR = "V:\\ML\\yandex-ml-2025\\src\\saves\\PoseCNNsc_13_24_35_#test1"
+SAVE_DIR = "V:\\ML\\yandex-ml-2025\\src\\saves\\PoseCNNsc_13_24_35_#test1"
+
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+NUM_EPOCHS = 75
+
+SCHEDULER = 'CosineAnnealingLR'
+
+WEIGHT_DECAY = 3e-3
+
+TRAIN_AUGMENTATION_TYPE = "basic"
+VALID_AUGMENTATION_TYPE = None
