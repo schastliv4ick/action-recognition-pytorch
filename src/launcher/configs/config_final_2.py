a17cdfbe08735990b0b17dea9136b815e7b16fa3
GrygoryZach <grishazschepilo@gmail.com>
2025-04-21 23:20:35 +0200

    feat: config and stage final2

---
+++ src/launcher/configs/config_final_2.py
@@ -0,0 +1,16 @@
+PATH_TO_DATA = "C:\\Users\\Semyon\\YandexLyceum\\project\\yandex-ml-2025\\data\\human_poses_data"
+RESULT_DIR = "C:\\Users\\Semyon\\YandexLyceum\\project\\yandex-ml-2025\\src\\saves\\final1"
+SAVE_DIR = "C:\\Users\\Semyon\\YandexLyceum\\project\\yandex-ml-2025\\src\\saves\\final1"
+
+
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+MOMENTUM = 0.9
+NUM_EPOCHS = 2
+
+SCHEDULER = 'CosineAnnealingLR'
+
+WEIGHT_DECAY = 3e-3
+
+TRAIN_AUGMENTATION_TYPE = "basic"
+VALID_AUGMENTATION_TYPE = None
--- src/launcher/models/__all_models.py
+++ src/launcher/models/__all_models.py
@@ -4,3 +4,4 @@ from .stage3 import PoseCNNsc as PoseCNNsc_stage3
 from .stage111 import PoseCNNsc as PoseCNNsc_stage111
 from .stage7 import DensePoseCNN as DensePoseCNN_stage7
 from .stage10 import PoseCNNv2 as PoseCNNv2_stage10
+from .stage_final_2 import PoseCNNsc_13_24_35 as PoseCNNsc_13_24_35_final2
