3fd8c2bf41d156e63bf0f64279a668021ed16649
Vlad-Akimov <121798983+Vlad-Akimov@users.noreply.github.com>
2025-04-22 01:13:54 +0300

    model final test 1

--- src/launcher/launcher.py
+++ src/launcher/launcher.py
@@ -7,16 +7,16 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from launched_trainer import train_model
-import models.__all_models as all_models
+from src.launcher.launched_trainer import train_model
+import src.launcher.models.__all_models as all_models
 
 # Uploading configs of models to train
-from configs import config1, config2, config3, config4, config5, config7, config8, config9, config_final_2
+from src.launcher.configs import config1, config2, config3, config4, config5, config7, config8, config9, config_final_2
 
 # Setting up models and configs
 launch_list = [
-    {"model": all_models.PoseCNNsc_stage111, "config": config4},
-    {"model": all_models.PoseCNNsc_13_24_35_final2, "config": config_final_2}
+    {"model": all_models.PoseCNNsc_13_24_35_stage1, "config": config1},
+    # {"model": all_models.PoseCNNsc_13_24_35_final2, "config": config_final_2}
 ]
 if __name__ == "__main__":
     today = datetime.today().strftime("%d.%m")
@@ -29,6 +29,5 @@ if __name__ == "__main__":
         config = entry['config']
         model_class = entry['model']
 
-        # train_model(config=config, model_class=model_class, rare_classes_threshold=0.025)
-        train_model(config, model_class)
-
+        train_model(config=config, model_class=model_class, rare_classes_threshold=0.025)
+        # train_model(config, model_class)
