58b9cc40ce0602979268fcf3ef8a95ac46da9a0c
GrygoryZach <grishazschepilo@gmail.com>
2025-04-22 01:20:45 +0200

    feat: excluding different classes for different models

--- src/launcher/launcher.py
+++ src/launcher/launcher.py
@@ -14,10 +14,19 @@ import src.launcher.models.__all_models as all_models
 from src.launcher.configs import config1, config2, config3, config4, config5, config7, config8, config9, config_final_2
 
 # Setting up models and configs
+
+classes_to_exclude_1 = ['inactivity quiet/light', 'religious activities', 'running',
+                        'self care', 'volunteer activities', 'transportation']  # 6 classes excluded, 14 classes left
+
+classes_to_exclude_2 = ['inactivity quiet/light', 'religious activities', 'running',
+                        'self care', 'volunteer activities', 'transportation',
+                        'walking', 'dancing', 'music playing', 'bicycling']  # 10 classes excluded, 10 classes left
+
 launch_list = [
-    {"model": all_models.PoseCNNsc_13_24_35_stage1, "config": config1},
-    # {"model": all_models.PoseCNNsc_13_24_35_final2, "config": config_final_2}
+   {"model": all_models.PoseCNNsc_13_24_35_stage1, "config": config1, "classes_to_exclude": classes_to_exclude_1},
+   {"model": all_models.PoseCNNsc_13_24_35_final2, "config": config_final_2, "classes_to_exclude": classes_to_exclude_2}
 ]
+
 if __name__ == "__main__":
     today = datetime.today().strftime("%d.%m")
 
@@ -28,9 +37,7 @@ if __name__ == "__main__":
 
         config = entry['config']
         model_class = entry['model']
-
-        classes_to_exclude = ['inactivity quiet/light', 'religious activities', 'running',
-                              'self care', 'volunteer activities', 'transportation']
+        classes_to_exclude = entry['classes_to_exclude']
 
         train_model(config=config, model_class=model_class, classes_to_exclude=classes_to_exclude)
         # train_model(config, model_class)
